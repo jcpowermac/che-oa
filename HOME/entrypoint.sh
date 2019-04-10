6e3ba8999032e36ef7aabf2050725743c9783666
Joseph Callen <jcallen@redhat.com>
2019-04-10 10:32:44 -0400

    readd group and passwd sub

--- HOME/entrypoint.sh
+++ HOME/entrypoint.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/bin/bash" >> /etc/passwd
-  fi
+set -e
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+if ! grep -Fq "${USER_ID}" /etc/passwd; then
+    # current user is an arbitrary
+    # user (its uid is not in the
+    # container /etc/passwd). Let's fix that
+    cat ${HOME}/passwd.template | \
+    sed "s/\${USER_ID}/${USER_ID}/g" | \
+    sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
+    sed "s/\${HOME}/\/home\/user/g" > /etc/passwd
+
+    cat ${HOME}/group.template | \
+    sed "s/\${USER_ID}/${USER_ID}/g" | \
+    sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
+    sed "s/\${HOME}/\/home\/user/g" > /etc/group
 fi
+
+is_current_user_sudoer() {
+    sudo -n true > /dev/null 2>&1
+}
 
 exec "$@"
